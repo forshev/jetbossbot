4304497bd487d05c7369b6ff8519b13ccf015c5b
forshev <xdannyxwalkerx@gmail.com>
2018-02-03 02:18:06 +0500

    more handlers

--- bot.py
+++ bot.py
@@ -4,13 +4,13 @@ import telebot
 
 from datetime import datetime
 from messages import *
-from utils import init_keyboard
+from utils import *
 
 
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=['start'])
 def start_handler(message):
     markup = init_keyboard()
     reply = start_message
@@ -18,8 +18,13 @@ def start_handler(message):
     bot.send_message(message.chat.id, reply, reply_markup=markup)
 
 
-@bot.message_handler(commands=["came"])
-def came_handler(message):
+@bot.message_handler(commands=['help'])
+def help_handler(message):
+    bot.send_message(message.chat.id, help_message)
+
+
+@bot.message_handler(commands=['came', 'left'])
+def came_left_handler(message):
     full_name = '{} {}'.format(
         message.chat.first_name,
         message.chat.last_name
@@ -27,11 +32,35 @@ def came_handler(message):
     time = datetime.fromtimestamp(
         message.date
     ).strftime('%H:%M')
-    reply = came_message.format(full_name, time)
+
+    if message.text == '/came':
+        reply = came_message.format(full_name, time)
+    elif message.text == '/left':
+        reply = left_message.format(full_name, time)
 
     bot.send_message(message.chat.id, reply)
 
 
+@bot.message_handler(commands=['location'])
+def request_location(message):
+    markup = location_keyboard()
+
+    bot.send_message(
+        message.chat.id,
+        "Поделись местоположением",
+        reply_markup=markup)
+
+    bot.register_next_step_handler(message, location_handler)
+
+
+@bot.message_handler(content_types=['location'])
+def location_handler(message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+
+    print(lat, lon)
+
+
 if __name__ == '__main__':
-    print("Bot is running...")
+    print('Bot is running...')
     bot.polling(none_stop=True)
--- utils.py
+++ utils.py
@@ -3,9 +3,26 @@ from telebot import types
 
 
 def init_keyboard():
-    markup = types.ReplyKeyboardMarkup()
+    markup = types.ReplyKeyboardMarkup(
+        row_width=1, resize_keyboard=True
+    )
 
     markup.add('/came')
     markup.add('/left')
+    markup.add('/location')
+
+    return markup
+
+
+def location_keyboard():
+    markup = types.ReplyKeyboardMarkup(
+        row_width=1, resize_keyboard=True
+    )
+    location_button = types.KeyboardButton(
+        text="Отправить местоположение",
+        request_location=True
+    )
+
+    markup.add(location_button)
 
     return markup
